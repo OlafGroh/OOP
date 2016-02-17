1b5be83ea1c4ad1343340657d7d09df4c471166d
Olaf Groh <Olaf.Groh@web.de>
2016-02-17 10:27:14 +0100

    Schönheitskorrekturen/eye candy

--- C#/Gehoelz.cs
+++ C#/Gehoelz.cs
@@ -1,21 +1,21 @@
 using System;
 
 public abstract class Gehoelz {
-	
+
 	/* Attribute der abstrakten Basisklasse */
 	/* Attriubutes of the abstract base class */
-	private String	art;
-	private int		pflanzjahr;
-	private float	preis;
-	
+	private String		art;
+	private int				pflanzjahr;
+	private float			preis;
+
 	/* Konstruktor:Alle Parameter in den Attrubuten der Klasse speichern */
-	/* Constructor:store all parameters in the attributes of the class */	 
+	/* Constructor:store all parameters in the attributes of the class */
 	public Gehoelz(String art, int pflanzjahr, float preis) {
 		this.art		= art;
 		this.pflanzjahr	= pflanzjahr;
 		this.preis		= preis;
 	}
-	
+
 	/* Get- und Set Methoden für alle Attribute der Klasse */
 	/* Get and set methodes for all attrubutes of the class */
 	public String getArt() {
@@ -41,10 +41,10 @@ public abstract class Gehoelz {
 	public void setPreis(float preis) {
 		this.preis = preis;
 	}
-	
+
 	/* Methode wird in allen Subklassen (ausser Liane) überschrieben */
 	/* Method are overwritten in all (not in Liane) sub classes */
 	public String getInfo() {
 		return "Art: " +this.art + " Pflanzjahr: " + this.pflanzjahr + " Preis: " + this.preis;
-	}	
+	}
 }
