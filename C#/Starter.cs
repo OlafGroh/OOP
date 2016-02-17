0c462b7f968dad3b26513601a039800f4fa8aa5e
Olaf Groh <Olaf.Groh@web.de>
2016-02-17 21:43:14 +0100

    Klasse Gaertnerei hinzugefügt und sicheren Cast eingebaut/add class Gaertnerei an safe cast add

--- C#/Starter.cs
+++ C#/Starter.cs
@@ -1,5 +1,5 @@
 using System;
-
+using System.Collections.Generic;
 /*
  * autor:   Olaf Groh
  * date:    17/02/2016
@@ -20,5 +20,50 @@ public class Starter
       Console.WriteLine("Baum: "    + b2.getInfo());
       Console.WriteLine("Liane: "   + l1.getInfo());
       Console.WriteLine("Strauch: " + s1.getInfo());
+
+      /* Objekt vom Typ Gaertnerei erstellen */
+      /* create an object from type Gaertnerei */
+      Gaertnerei gae1 = new Gaertnerei();
+
+      /* testen der einkaufen Methode einkaufen */
+      /* test the einkaufen method */
+      gae1.einkaufen(b1);
+      gae1.einkaufen(b2);
+      gae1.einkaufen(l1);
+      gae1.einkaufen(s1);
+
+      /* testen der verkaufen Methode einkaufen */
+      /* test the verkaufen method */
+      gae1.verkaufen("Eiche" , 2012);
+
+      /* testen der getWoodByLimit Methode einkaufen */
+      /* test the getWoodByLimit method */
+      List<Gehoelz> tmp;
+      tmp = gae1.getWoodByLimit(199.01f);
+
+      /* testen ob die Methode die richtigen Werte zurück liefert */
+      /* test if the returnvalues of the method are right */
+      Console.WriteLine();
+      for(int c=0; c<tmp.Count; c++) {
+        Console.WriteLine("[" + c + "] = " + tmp[c].getInfo() );
+      }
+
+      /* die Methode erweiterungen der Klassen Baum und Strauch testen */
+      /* test of the extension of the classes Baum and Strauch */
+      Console.WriteLine();
+      for(int c=0; c<tmp.Count; c++) {
+        Gehoelz objtmp = tmp[c];
+        if( objtmp is Baum) {
+            Baum baum = (Baum)objtmp;
+            Console.WriteLine("Instanz war vom Typ Baum: Art=" + baum.getArt() + " maximale Höhe=" + baum.getMaxHoehe() );
+        }else if( objtmp is Liane) {
+          Liane liane = (Liane)objtmp;
+          Console.WriteLine("Instanz war vom Typ Liane : Art=" + liane.getArt() );
+        }else if( objtmp is Strauch) {
+          Strauch strauch = (Strauch)objtmp;
+          Console.WriteLine("Instanz war vom Typ Strauch : Art=" + strauch.getArt() + "ist Giftig=" + strauch.getIstGiftig() );
+        }
+      }
+
     }
 }
