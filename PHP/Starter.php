18ced4fb2ff45f06a357d9292927ad698596fbd9
Olaf Groh <Olaf.Groh@web.de>
2016-02-17 15:10:27 +0100

    Schönheitskorrekturen/eye candy

--- PHP/Starter.php
+++ PHP/Starter.php
@@ -12,15 +12,15 @@ function main() {
 
   /* einige Testdaten */
   /* some testdata */
-	$b1 = new Baum("Eiche", 2012, 299, 25);
-	$b2 = new Baum("Pappel", 2010, 199, 15);
-	$l1 = new Liane("Gemeiner Efeu", 1990, 15);
-	$s1 = new Strauch("Stechapfel", 1999, 2.5, true);
+  $b1 = new Baum("Eiche", 2012, 299, 25);
+  $b2 = new Baum("Pappel", 2010, 199, 15);
+  $l1 = new Liane("Gemeiner Efeu", 1990, 15);
+  $s1 = new Strauch("Stechapfel", 1999, 2.5, true);
 
-	print "Baum: " . $b1->getInfo()  . "\n";
-	print "Baum: " . $b2->getInfo()  . "\n";
-	print "Liane: " . $l1->getInfo() . "\n";
-	print "Strauch: " . $s1->getInfo() . "\n";
+  print "Baum: "    . $b1->getInfo()  . "\n";
+  print "Baum: "    . $b2->getInfo()  . "\n";
+  print "Liane: "   . $l1->getInfo() . "\n";
+  print "Strauch: " . $s1->getInfo() . "\n";
 }
 
 /* Einstiegspunkt wie z.B in C/C++ und Java nachbilden */
